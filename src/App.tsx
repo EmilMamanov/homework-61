9c80b5e7a3e150a3744ccdb90e92dd91c91a2c62
Mamanov Emil <emil.mamanov97@gmail.com>
2023-11-22 22:24:30 +0600

    started project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const handleSelectCountry = (countryCode: string) => {
+        setSelectedCountry(countryCode);
+    };
 
-export default App
+    return (
+        <div>
+            <div>left side</div>
+            <div>right side</div>
+        </div>
+    );
+};
+
+export default App;
